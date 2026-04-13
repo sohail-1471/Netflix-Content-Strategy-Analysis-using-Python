5badcf338726b41bc2b69a484fb2242f28de57f5
sohail-1471 <sohailzareen279@gmail.com>
2026-04-14 01:13:33 +0500

    Update netflix_content_analysis.py

--- netflix_content_analysis.py
+++ netflix_content_analysis.py
@@ -10,7 +10,7 @@ import plotly.graph_objects as go
 import plotly.io as pio
 pio.templates.default = "plotly_white"
 
-netflix_data = pd.read_csv("D:/Data Science/Data Science projects/data analysis/Netflix_content_strategy/netflix_content_2023.csv")
+netflix_data = pd.read_csv("netflix_content_2023.csv")
 
 print(netflix_data.head())
 
